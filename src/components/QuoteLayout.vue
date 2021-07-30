f5052a0a199afaf3b927da45dbd1626acfda2430
lawrencegorris <gorrislawrence.personal@gmail.com>
2021-07-30 15:30:08 +0200

    Updated Styling

--- src/components/QuoteLayout.vue
+++ src/components/QuoteLayout.vue
@@ -1,5 +1,5 @@
 <template>  
-    <p class="text-yellow-300 font-medium text-xl md:px-10 p-5">{{quote}} <br><br> - {{character}}, <span class="italic">{{anime}}</span></p>
+    <p class="text-yellow-300 font-medium text-xl p-5 sm:px-40 sm:mt-20 lg:mt-32">{{quote}} <br><br> - {{character}}, <span class="italic">{{anime}}</span></p>
 </template>
 
 <script>
